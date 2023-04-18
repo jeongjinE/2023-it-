cff3e335db6f324d057fdd835dd7d4eae18661a4
JeongJin <105847234+jeongjinE@users.noreply.github.com>
2023-04-18 23:04:19 +0900

    Add files via upload

---
+++ None1 .java
@@ -0,0 +1,15 @@
+package Week6;
+
+public class None1 {
+	public static void main(String[] args) {
+		
+	for (int i = 2; i <= 6; i++) {
+		
+		for (int j = i; j <= i + 4; j++) {
+			
+		System.out.print(j + " ");
+		}
+			System.out.println();
+	}
+}
+}
